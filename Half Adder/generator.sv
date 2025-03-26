e9a0251d9ca219d898bedccd52022e0eecf62c65
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-03-26 23:13:46 +0530

    Create generator.sv

---
+++ Half Adder/generator.sv
@@ -0,0 +1,27 @@
+// `include "transaction.sv"
+class generator;
+  
+  transaction tr;
+  event my_event;
+  mailbox gen2drv;
+  
+  function new (mailbox nithish);
+    this.gen2drv=nithish;
+  endfunction
+  
+  task main();
+    tr=new();
+    repeat(10)
+      begin
+        
+        tr.randomize();
+       // $display("------------------------------------------");
+        tr.display("Generator Class Signals");
+        gen2drv.put(tr);
+        
+        
+        @(my_event);
+      end
+  endtask
+  
+endclass
