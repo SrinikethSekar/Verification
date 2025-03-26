f32039e020cdeb08cf4e2957abd61ae3039274c6
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-03-26 23:14:13 +0530

    Create driver.sv

---
+++ Half Adder/driver.sv
@@ -0,0 +1,29 @@
+class driver;
+  
+  virtual int_if vi;
+  transaction tr;
+  mailbox gen2drv;
+  
+  function new(virtual int_if vi,mailbox valan);
+    
+    this.vi=vi;
+    this.gen2drv=valan;
+    
+  endfunction
+  
+  task main();
+    repeat(10)
+      begin
+               
+
+        gen2drv.get(tr);
+        vi.a=tr.a;
+        vi.b=tr.b;
+        
+        tr.display("Driver Class Signals");
+        #1;
+      end
+  endtask
+ 
+endclass
+        
