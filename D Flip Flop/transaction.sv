0d260bb8736c894779fee8d24f3ea8fc4b8a3759
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-04-01 23:25:28 +0530

    Update transaction.sv

--- D Flip Flop/transaction.sv
+++ D Flip Flop/transaction.sv
@@ -1,14 +1,13 @@
 class transaction;
   
   rand bit data;
-  		bit clk;
-  		bit rst;
+       bit rst;
   		bit q_b;
   		bit q;
   
-  function new(string name);
+  function void display(string name);
     $display("%s",name);
-    $display("Time=%0t clk=%b Data=%b Q=%b Q_b=%b",$time,clk,data,q,q_b);
+    $display("Time=%0t Data=%b Q=%b Q_b=%b",$time,data,q,q_b);
   endfunction
 
 endclass
