9cfa4eec0ee52c0d9bdece9cd619e8132b3e2915
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-04-03 22:40:58 +0530

    Update transaction.sv

--- SRAM/transaction.sv
+++ SRAM/transaction.sv
@@ -6,11 +6,14 @@ class transaction;
   rand bit [2:0] addr;
   rand bit [7:0] data_in;
        bit [7:0] data_out;
-  constraint valid{addr inside {[0:7]};}
+  
+  constraint valid_we{we dist{1:=60,0:=40};} 
+  constraint valid_addr{addr inside {[0:7]};}
+  constraint valid_data_in{data_in inside {[8'h00:8'hFF]};}
   
   function void display(string name);
     $display("%s",name);
-    $display("Time=%0t WE=%b ADDR=%b DATA_IN=%b DATA_OUT=%b",$time,we,addr,data_in,data_out);
+    $display("Time=%0t WE=%b ADDR=%d DATA_IN=%d DATA_OUT=%d",$time,we,addr,data_in,data_out);
   endfunction
   
 endclass
