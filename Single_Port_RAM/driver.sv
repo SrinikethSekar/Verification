b273d3a2eba67f17e78fd1d559076e71b335d28d
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-05-03 22:45:48 +0530

    Create driver.sv

---
+++ Single_Port_RAM/driver.sv
@@ -0,0 +1,40 @@
+class driver;
+  
+  transaction tr;
+  mailbox gen2drv;
+  virtual sram_if sr;
+  
+  function new(virtual sram_if sr,mailbox gen2drv);
+    this.sr=sr;
+    this.gen2drv=gen2drv;
+  endfunction
+  
+  task main();
+    tr=new();
+   
+    repeat(tr.val)
+      begin
+        
+        
+       
+        gen2drv.get(tr);
+      
+        @(posedge sr.clk)
+        #2; 
+      
+        sr.we     <= tr.we;
+        sr.data_in <= tr.data_in;
+        sr.addr     <= tr.addr;
+      
+        //$display("[DRIVER] Applied transaction at Time=%0t | WE=%b | ADDR=%b | DATA_IN=%b",$time, tr.we, tr.addr, tr.data_in);
+
+        tr.display("[DRIVER SIGNALS]");
+        
+       
+        
+        
+      end
+    
+  endtask
+  
+endclass
